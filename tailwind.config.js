7a15a1ba48ad62603dc7a769f691afc807446b10
RM POS <indime.hn@gmail.com>
2025-09-07 22:02:13 -0600

    Add Tailwind CSS configuration with custom settings

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#1e40af',
+      },
+      spacing: {
+        72: '18rem',
+      },
+      screens: {
+        xs: '400px',
+      },
+      width: {
+        'custom-w': '350px',
+      },
+      minWidth: {
+        'custom-min-w': '200px',
+      },
+      minHeight: {
+        'custom-min-h': '300px',
+      },
+    },
+  },
+  plugins: [],
+}
